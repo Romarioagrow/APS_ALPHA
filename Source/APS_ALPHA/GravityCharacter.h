c11c448d3ef39c822d8d0c792313dfcdad579de3
Romarioagrow <Romarioagrow@mail.ru>
2023-04-18 17:55:00 +0300

    Gravity Controller

--- Source/APS_ALPHA/GravityCharacter.h
+++ Source/APS_ALPHA/GravityCharacter.h
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
+
 #include "GravityTypeEnum.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
