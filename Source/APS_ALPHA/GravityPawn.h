91f58515cd336aeaba8fc1111502e44253b4a63c
Romarioagrow <Romarioagrow@mail.ru>
2023-05-01 02:50:19 +0300

    GravityPawn Interface

--- Source/APS_ALPHA/GravityPawn.h
+++ Source/APS_ALPHA/GravityPawn.h
@@ -22,41 +22,10 @@ class APS_ALPHA_API IGravityPawn
 {
 	GENERATED_BODY()
 
-	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
-
-	//virtual void UpdateGravity();
-
-	//virtual void UpdateGravityStatus(AActor* OtherActor);
-
-	//virtual FVector GetGravityDirection() const;
-
 	virtual void UpdateGravity() = 0;
 
 	virtual void UpdateGravityType() = 0;
 
 	virtual void SwitchGravityType(AActor* GravitySourceActor) = 0;
-
-protected:
-	//EGravityType CurrentGravityType{
-	//	EGravityType::ZeroG
-	//};
-
-	//TScriptInterface<IGravitySource> GravityTargetActor;
-	////TScriptInterface<IGravityObject>
-
-	//FVector GravityDirection{
-	//	0.0f, 0.0f, 0.0f
-	//};
-
-	
-
-private:
-	/*virtual void UpdateZeroGGravity();
-
-	virtual void UpdateStationGravity();
-
-	virtual void UpdatePlanetGravity();
-
-	virtual void UpdateShipGravity();*/
 };
