dd47d5053b44d5041b0f25cd64896cb8fec94e80
Romarioagrow <Romarioagrow@mail.ru>
2023-05-23 02:24:08 +0300

    GeneratePlanetraySystemModelByStar Orbits

--- Source/APS_ALPHA/PlanetarySystemGenerationModel.h
+++ Source/APS_ALPHA/PlanetarySystemGenerationModel.h
@@ -1,9 +1,30 @@
 #pragma once
 #include "PlanetarySystemType.h"
+#include "Containers/UnrealString.h" 
+#include "OrbitDistributionType.h" 
 
 #include "GenerationModel.h"
 #include "CoreMinimal.h"
 #include "PlanetarySystemGenerationModel.generated.h"
+
+
+//USTRUCT(BlueprintType)
+//struct FZoneRadius
+//{
+//	GENERATED_USTRUCT_BODY()
+//
+//		UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Zone")
+//		double InnerRadius;
+//
+//	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Zone")
+//		double OuterRadius;
+//
+//	FZoneRadius()
+//	{
+//		InnerRadius = 0.0;
+//		OuterRadius = 0.0;
+//	}
+//};
 
 USTRUCT(BlueprintType)
 struct FPlanetarySystemGenerationModel :
@@ -21,6 +42,80 @@ struct FPlanetarySystemGenerationModel :
 
     UPROPERTY(VisibleAnywhere)
         EPlanetarySystemType PlanetarySystemType;
+    
+    UPROPERTY(VisibleAnywhere)
+        EOrbitDistributionType OrbitDistributionType;
+
+	//UPROPERTY(VisibleAnywhere, Category = "Zones")
+	//	FZoneRadius HabitableZoneRadius;
+
+	//UPROPERTY(VisibleAnywhere)
+	//	FZoneRadius ColdZoneRadius;
+
+	//UPROPERTY(VisibleAnywhere)
+	//	FZoneRadius IceZoneRadius;
+
+	//UPROPERTY(VisibleAnywhere)
+	//	FZoneRadius WarmZoneRadius;
+
+	//UPROPERTY(VisibleAnywhere)
+	//	FZoneRadius HotZoneRadius;
+
+	//UPROPERTY(VisibleAnywhere)
+	//	FZoneRadius InnerPlanetZoneRadius;
+
+	//UPROPERTY(VisibleAnywhere)
+	//	FZoneRadius OuterPlanetZoneRadius;
+
+	////UPROPERTY(VisibleAnywhere)
+	//	//TMap<int32, FZoneRadius> AsteroidBeltZoneRadius;
+
+	//UPROPERTY(VisibleAnywhere)
+	//	FZoneRadius KuiperBeltZoneRadius;
+
+ //   //UPROPERTY(VisibleAnywhere)
+	//	TPair<double, double> HabitableZoneRadius;
+
+
+	//// Habitable Zone
+	////UPROPERTY(VisibleAnywhere)
+	//	TPair<double, double> HabitableZoneRadius;
+
+	//// Cold Zone
+	////UPROPERTY(VisibleAnywhere)
+	//	TPair<double, double> ColdZoneRadius;
+
+	//// Ice Zone
+	////UPROPERTY(VisibleAnywhere)
+	//	TPair<double, double> IceZoneRadius;
+
+	//// Warm Zone
+	////UPROPERTY(VisibleAnywhere)
+	//	TPair<double, double> WarmZoneRadius;
+
+	//// Hot Zone
+	////UPROPERTY(VisibleAnywhere)
+	//	TPair<double, double> HotZoneRadius;
+
+	//// Gas Giants Zone
+	////UPROPERTY(VisibleAnywhere)
+	//	TPair<double, double> GasGiantsZoneRadius;
+
+	//// Asteroid Belt Zone
+	////UPROPERTY(VisibleAnywhere)
+	//	TPair<double, double> AsteroidBeltZoneRadius;
+
+	//// Kuiper Belt Zone
+	////UPROPERTY(VisibleAnywhere)
+	//	TPair<double, double> KuiperBeltZoneRadius;
+
+	//// Inner Zone
+	////UPROPERTY(VisibleAnywhere)
+	//	TPair<double, double> InnerZoneRadius;
+
+	//// Outer Zone
+	////UPROPERTY(VisibleAnywhere)
+	//	TPair<double, double> OuterZoneRadius;
 
 };
 
