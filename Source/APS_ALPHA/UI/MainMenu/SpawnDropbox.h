a3d6fe7a354f4ec2a9cf6abf28e25bb31244ef20
Roman Moltanovski <Romarioagrow@mail.ru>
2024-08-02 22:08:24 +0500

    Spawn menu UI wip

---
+++ Source/APS_ALPHA/UI/MainMenu/SpawnDropbox.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "APS_ALPHA/UI/BaseWidget.h"
+#include "SpawnDropbox.generated.h"
+
+
+UCLASS()
+class USpawnDropbox : public UBaseWidget
+{
+	GENERATED_BODY()
+	
+public:
+	
+};
