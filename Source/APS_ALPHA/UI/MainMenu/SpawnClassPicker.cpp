0d23ea69e034e592bcf4e229841ab7a0f126c3ee
Roman Moltanovski <Romarioagrow@mail.ru>
2024-08-02 22:08:24 +0500

    Spawn menu UI wip

---
+++ Source/APS_ALPHA/UI/MainMenu/SpawnClassPicker.cpp
@@ -0,0 +1,66 @@
+﻿#include "SpawnClassPicker.h"
+
+#include "Components/TextBlock.h"
+
+void USpawnClassPicker::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	// Инициализация массива классов
+	/*SpaceshipClasses = {
+		BP_HomeSpaceHeadquarters::StaticClass(),
+		BP_HomeSpaceStation::StaticClass(),
+		BP_HomeSpaceship::StaticClass()
+	};
+
+	InitializeClassPicker();*/
+
+	SpaceshipClasses = {
+		// Замените на ваши конкретные классы
+		//BP_HomeSpaceship::StaticClass(),
+		//BP_HomeSpaceStation::StaticClass()
+	};
+
+	CharacterClasses = {
+		// Замените на ваши конкретные классы
+		//BP_CharacterClass::StaticClass()
+	};
+
+	SpaceStationClasses = {
+		// Замените на ваши конкретные классы
+		//BP_HomeSpaceHeadquarters::StaticClass()
+	};
+	
+}
+
+
+
+void USpawnClassPicker::InitializeClassPicker()
+{
+	// Очистка текущих элементов
+	//SpaceshipVerticalBox->ClearChildren();
+	//CharacterVerticalBox->ClearChildren();
+	//SpaceStationVerticalBox->ClearChildren();
+
+	for (TSubclassOf<AActor> Class : SpaceshipClasses)
+	{
+		//AddClassToVerticalBox(Class, SpaceshipVerticalBox);
+	}
+
+	for (TSubclassOf<AActor> Class : CharacterClasses)
+	{
+		//AddClassToVerticalBox(Class, CharacterVerticalBox);
+	}
+
+	for (TSubclassOf<AActor> Class : SpaceStationClasses)
+	{
+		//AddClassToVerticalBox(Class, SpaceStationVerticalBox);
+	}
+}
+
+/*void USpawnClassPicker::AddClassToVerticalBox(const TSubclassOf<AActor> Class, UVerticalBox* VerticalBox)
+{
+	//UTextBlock* TextBlock = NewObject<UTextBlock>(this);
+	//TextBlock->SetText(FText::FromString(Class->GetName()));
+	//VerticalBox->AddChild(TextBlock);
+}*/
---
+++ Source/APS_ALPHA/UI/MainMenu/SpawnClassPicker.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+#include "APS_ALPHA/UI/BaseWidget.h"
+#include "SpawnClassPicker.generated.h"
+
+class USpawnDropbox;
+
+UCLASS()
+class USpawnClassPicker : public UBaseWidget
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeConstruct() override;
+
+
+	/*
+	 * BP_HomeSpaceHeadquarters
+	 * BP_HomeSpaceStation
+	 * BP_HomeSpaceship
+	 * BP_CharacterClass
+	 */
+
+	UPROPERTY(meta = (BindWidget))
+	USpawnDropbox* CharacterDropbox;
+	
+	UPROPERTY(meta = (BindWidget))
+	USpawnDropbox* SpaceshipDropbox;
+
+	UPROPERTY(meta = (BindWidget))
+	USpawnDropbox* SpaceStationDropbox;
+
+	UPROPERTY(meta = (BindWidget))
+	USpawnDropbox* SpaceHeadquartersDropbox;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Classes")
+	TArray<TSubclassOf<AActor>> SpaceshipClasses;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Classes")
+	TArray<TSubclassOf<AActor>> CharacterClasses;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Classes")
+	TArray<TSubclassOf<AActor>> SpaceStationClasses;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Classes")
+	TArray<TSubclassOf<AActor>> SpaceHeadquartersClasses;
+
+	void InitializeClassPicker();
+	
+	//void AddClassToVerticalBox(TSubclassOf<AActor> Class, UVerticalBox* VerticalBox);
+};
